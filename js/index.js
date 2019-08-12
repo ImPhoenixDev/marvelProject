113b54ac50698ed857bfe8f9a5a22b460ace78de
Guillermo Loza <iguillermoloza@gmail.com>
2019-08-12 13:08:37 -0500

    Lo que ha costado, hay que convertir la respuesta a json y solo entonces se puede acceder uffff....  Ya esta funcionando

--- js/index.js
+++ js/index.js
@@ -3,10 +3,14 @@ console.log("Hola mundo");
 let ironMan =
   "https://gateway.marvel.com:443/v1/public/characters?nameStartsWith=iron%20man&ts=1&apikey=9247cc40ccabf6b7899f5b61ee20f4e3&hash=c7b493fe930ae3d81e59433eccb77ab9";
 
-fetch(ironMan)
-  .then(holaSoyElDatoQueTrajiste => {
-    console.log(holaSoyElDatoQueTrajiste);
-    document.write(holaSoyElDatoQueTrajiste);
+let ironManId =
+  "https://gateway.marvel.com:443/v1/public/characters/1009368?&ts=1&apikey=9247cc40ccabf6b7899f5b61ee20f4e3&hash=c7b493fe930ae3d81e59433eccb77ab9";
+fetch(ironManId)
+  .then(response => {
+    return response.json();
+  })
+  .then(json => {
+    console.log(json);
   })
 
   .catch(() => {
