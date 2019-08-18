1bf0e6b23fa4e63a5b6a7a448c39bd9bf5e76fbb
Guillermo Loza <iguillermoloza@gmail.com>
2019-08-18 17:45:12 -0500

    hola

--- js/index.js
+++ js/index.js
@@ -5,7 +5,13 @@ let ironMan =
 
 let ironManId =
   "https://gateway.marvel.com:443/v1/public/characters/1009368?&ts=1&apikey=9247cc40ccabf6b7899f5b61ee20f4e3&hash=c7b493fe930ae3d81e59433eccb77ab9";
-fetch(ironManId)
+
+let loki =
+  "https://gateway.marvel.com:443/v1/public/characters?name=Loki&ts=1&apikey=9247cc40ccabf6b7899f5b61ee20f4e3&hash=c7b493fe930ae3d81e59433eccb77ab9";
+
+let capi =
+  "https://gateway.marvel.com:443/v1/public/characters?name=Captain%20America&ts=1&apikey=9247cc40ccabf6b7899f5b61ee20f4e3&hash=c7b493fe930ae3d81e59433eccb77ab9";
+fetch(capi)
   .then(response => {
     return response.json();
   })
@@ -16,3 +22,24 @@ fetch(ironManId)
   .catch(() => {
     console.log("Algo anda mall ¬¬");
   });
+
+var test = "Hola que tal";
+function quitarEspacios(st) {
+  var link;
+  link = st.replace(/ /g, "%20");
+  return link;
+}
+
+function searchNameStart(inputBrowse) {
+  let nameStartsWithBrowse = quitarEspacios(inputBrowse);
+  var linkBrowse = `https://gateway.marvel.com:443/v1/public/characters?nameStartsWith=${nameStartsWithBrowse}&ts=1&apikey=9247cc40ccabf6b7899f5b61ee20f4e3&hash=c7b493fe930ae3d81e59433eccb77ab9`;
+  return linkBrowse;
+}
+
+function getHeroName() {
+  let inputUser = prompt("Como empieza el nombre de tu heroe?", "Iron Man");
+  if (inputUser != null) {
+    search = searchNameStart(inputUser);
+    fetch(search);
+  }
+}
