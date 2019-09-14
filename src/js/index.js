cb348b7ea17ac7c6830c0dfc903c344c9fd57068
Guillermo Loza <iguillermoloza@gmail.com>
2019-09-14 13:30:02 -0500

    Webpack start config

--- src/js/index.js
+++ src/js/index.js
@@ -11,19 +11,7 @@ let loki =
 
 let capi =
   "https://gateway.marvel.com:443/v1/public/characters?name=Captain%20America&ts=1&apikey=9247cc40ccabf6b7899f5b61ee20f4e3&hash=c7b493fe930ae3d81e59433eccb77ab9";
-fetch(capi)
-  .then(response => {
-    return response.json();
-  })
-  .then(json => {
-    console.log(json);
-  })
 
-  .catch(() => {
-    console.log("Algo anda mall ¬¬");
-  });
-
-var test = "Hola que tal";
 function quitarEspacios(st) {
   var link;
   link = st.replace(/ /g, "%20");
@@ -40,6 +28,22 @@ function getHeroName() {
   let inputUser = prompt("Como empieza el nombre de tu heroe?", "Iron Man");
   if (inputUser != null) {
     search = searchNameStart(inputUser);
-    fetch(search);
+    callAPI(search);
   }
 }
+
+function callAPI(search) {
+  fetch(search)
+    .then(response => {
+      return response.json();
+    })
+    .then(json => {
+      console.log(json);
+    })
+
+    .catch(() => {
+      console.log("Algo anda mall ¬¬");
+    });
+}
+
+getHeroName();
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/js/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "./js/bunde.js"
+  },
+  resolve: {
+    extensions: [".js", ".jsx"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.styl$/,
+        use: ["style-loader", "css-loader", "stylus-loader"]
+      },
+      {
+        test: /\.html$/,
+        use: {
+          loader: "html-loader"
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./public/index.html",
+      filename: "./index.html"
+    })
+  ]
+};
