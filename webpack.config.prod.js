2639d8a84193ecd0384993e8c0f3f8711dd65eec
Guillermo Loza <iguillermoloza@gmail.com>
2019-10-21 19:41:39 -0500

    test

---
+++ webpack.config.prod.js
@@ -0,0 +1,61 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+module.exports = {
+  entry: {
+    app: path.resolve(__dirname, "src/index.js")
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "js/[name].js",
+    publicPath: "http://localhost:3001/",
+    chunkFilename: "js/[id].[chunkhash].js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: "babel-loader",
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader
+          }
+        ]
+      },
+      {
+        test: /\.styl$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader
+          }
+        ]
+      },
+      {
+        test: /\.jpg|png|gif|woff|eot|ttf|svg|mp4|webm$/,
+        use: {
+          loader: "url-loader",
+          options: {
+            limit: 1000
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "css/[name].css",
+      chunkFilename: "css/[id].css"
+    }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "public/index.html")
+    }),
+    new webpack.DllReferencePlugin({
+      manifest: require("./modules-manifest.json")
+    })
+  ]
+};
