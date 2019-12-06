3ab2fec7f83c62c6584fc738d17d937a7f736307
Guillermo Loza <iguillermoloza@gmail.com>
2019-12-06 16:47:49 -0600

    fix: Now it's secure

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -4,11 +4,14 @@ import "../assets/styles/components/Events.styl";
 
 export default function Events(props) {
   const { eventsURL } = props;
-  const path = `${eventsURL}?ts=1&apikey=9247cc40ccabf6b7899f5b61ee20f4e3&hash=c7b493fe930ae3d81e59433eccb77ab9&limit=50`;
-  const completePath = path.replace(/http/, https);
-  const eventsData = useEvents(completePath);
 
+  const eventsURLSecurePath = `${eventsURL}?ts=1&apikey=9247cc40ccabf6b7899f5b61ee20f4e3&hash=c7b493fe930ae3d81e59433eccb77ab9&limit=50`;
+  const completePath = eventsURLSecurePath.replace(/http/, "https");
+
+  console.log(completePath);
+  const eventsData = useEvents(completePath);
   console.log(eventsData);
+
   return (
     <section className="wrapper">
       <div className="info">
@@ -23,7 +26,10 @@ export default function Events(props) {
             return (
               <div className="event" key={results.id}>
                 <img
-                  src={`${results.thumbnail.path}.${results.thumbnail.extension}`}
+                  src={`${results.thumbnail.path.replace(
+                    /^http:\/\//i,
+                    "https://"
+                  )}.${results.thumbnail.extension}`}
                   alt={`${results.title} image`}
                   className="thumbnail"
                 />
